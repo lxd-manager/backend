bea99c928aad7af850696596b3ccec5490166f91
Felix Engelmann <fe-github@nlogn.org>
2020-03-05 18:29:42 +0100

    cacert

--- ct_backend/settings/lxd.py
+++ ct_backend/settings/lxd.py
@@ -4,3 +4,8 @@ LXD_CRT = os.environ.get('LXD_CRT', 'certs/lxd.crt')
 LXD_KEY = os.environ.get('LXD_KEY', 'certs/lxd.key')
 
 LXD_CA_CERT = os.environ.get('LXD_CA_CERT', 'certs/fakelerootx1.pem')
+
+if LXD_CA_CERT == 'False':
+    LXD_CA_CERT = False
+elif LXD_CA_CERT == 'True':
+    LXD_CA_CERT = True
