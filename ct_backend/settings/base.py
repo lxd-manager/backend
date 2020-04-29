41e48243c1f85978ac1dc7edc5f2eb2ef3ec8da8
Felix Engelmann <fe-github@nlogn.org>
2020-04-29 15:31:58 +0200

    CSRF flag

--- ct_backend/settings/base.py
+++ ct_backend/settings/base.py
@@ -53,7 +53,12 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+]
+if not os.environ.get('DJANGO_CSRF_EXCEPT', False):
+    MIDDLEWARE += [
+        'django.middleware.csrf.CsrfViewMiddleware',
+    ]
+MIDDLEWARE += [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
